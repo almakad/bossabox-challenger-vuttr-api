b9e47e528ed6960ea63fa7f5c7e1969ef0f5f6cd
almakad <carls-f@live.com>
2021-03-04 16:31:11 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -3,16 +3,19 @@ import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors'
 
-
 import { router } from '../routes/router'
 import  createConnection from '../../database/index'
 import { AppError } from '../../errors/AppError';
+
+import setupSwagger from './config-swagger'
 
 createConnection().then(() => console.log('Connected to database'))
   .catch((error) => console.log(error))
 
 
 const app = express();
+
+setupSwagger(app)
 
 app.use(express.json())
 
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'VUTTR (Very Useful Tools to Remember)',
+    description: 'To save tools in database',
+    version: '1.0.0'
+  }
+}
